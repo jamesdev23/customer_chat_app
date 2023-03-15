933a2002f6a7ad8adc068d6d24eb6519e99b17b1
James Nico Rara <113577747+jamesdev23@users.noreply.github.com>
2023-03-15 18:25:19 +0800

    added empty_button.xml changed customer list changed SignInActivity.kt

--- app/src/main/java/com/example/customerchatapp/SignInActivity.kt
+++ app/src/main/java/com/example/customerchatapp/SignInActivity.kt
@@ -11,6 +11,7 @@ import com.example.customerchatapp.databinding.ActivitySignInBinding
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
